1f9421c28fd5e2b8cf9f58f7fedaaeebd95f3b8c
Aleksey Shaydullin <101606512+AlekseyShaydullin@users.noreply.github.com>
2023-06-06 19:57:18 +0300

    feat: OrderCompletedPage

--- frontend/src/pages/OrderCompletedPage/OrderCompletedPage.tsx
+++ frontend/src/pages/OrderCompletedPage/OrderCompletedPage.tsx
@@ -1,11 +1,16 @@
 import { FC } from 'react';
 import style from './OrderCompletedPage.module.css';
+import goodJob from '../../images/good-job.jpg';
+import { Link } from 'react-router-dom';
 
 const OrderCompletedPage: FC = () => {
   return (
-    <>
-      <h1>Хорошая работа!</h1>
-    </>
+    <section className={style.wrapper}>
+      <img className={style.img} src={goodJob} alt='Выполненая работа' />
+      <h2 className={style.title}>Хорошая работа!</h2>
+      <p className={style.description}>Продолжайте упаковывать заказы</p>
+      <Link className={style.link} to='/'>ГОТОВО</Link>
+    </section>
   )
 }
 
