5b8501fa0e33ce4ec65f02a2286be807851bece3
Aleksey Shaydullin <101606512+AlekseyShaydullin@users.noreply.github.com>
2023-06-06 21:59:14 +0300

    feat: added form

---
+++ frontend/src/components/Progressbar/Progressbar.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import style from './Progressbar.module.css'
+
+const Progressbar: FC = () => {
+  return (
+    <article className={style.wrapper}>
+      <p>Прогрессбар</p>
+    </article>
+  )
+}
+
+export default Progressbar;
--- frontend/src/pages/PrinterSelectionPage/PrinterSelectionPage.tsx
+++ frontend/src/pages/PrinterSelectionPage/PrinterSelectionPage.tsx
@@ -1,10 +1,28 @@
 import { FC } from 'react';
 import style from './PrinterSelectionPage.module.css'
+import Footer from '../../components/Footer/Footer';
+import Progressbar from '../../components/Progressbar/Progressbar';
 
 const PrinterSelectionPage: FC = () => {
   return (
     <>
-      <h1>Отсканируйте штрихкод принтера или введите вручную</h1>
+      <section className={style.wrapper}>
+        <Progressbar />
+        <h2 className={style.title}>Отсканируйте штрихкод принтера или введите вручную</h2>
+        <form className={style.form}>
+          <input
+            className={style.input}
+            type='text'
+            placeholder=''
+            required
+          />
+          <div className={style.btns}>
+            <button className={style.btnBack}>Назад</button>
+            <button className={style.btnForward}>Далее</button>
+          </div>
+        </form>
+      </section>
+      <Footer />
     </>
   )
 }
