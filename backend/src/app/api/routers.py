e5c231642da3c8c31f444f0654637e0c8715ab2b
Levon Gegamyan <levongeg@gmail.com>
2023-06-07 22:08:38 +0300

    add endpoint /orders

--- backend/src/app/api/routers.py
+++ backend/src/app/api/routers.py
@@ -1,4 +1,9 @@
 from fastapi import APIRouter
 
+from app.api.endpoints import order_router
+
 
 main_router = APIRouter()
+main_router.include_router(
+    order_router, prefix='/order', tags=['Orders']
+)
