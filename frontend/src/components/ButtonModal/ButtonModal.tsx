0b612fd5e55b4d0194102b0110bdea4660d3ff64
Vladislav Nikitin <vlad.nikitin1997@bk.ru>
2023-06-13 08:20:58 +0300

    feat: Toast, fix: button problem

---
+++ frontend/src/components/ButtonModal/ButtonModal.tsx
@@ -0,0 +1,24 @@
+import { FC, PropsWithChildren } from 'react';
+import style from './ButtonModal.module.css';
+
+interface ButtonModalProps extends PropsWithChildren {
+  title: string;
+  onClick: () => void;
+  isOpen: boolean;
+}
+
+const ButtonModal: FC<ButtonModalProps> = ({...props}) => {
+
+  return props.isOpen !== true ? (
+    <>
+    <button type='button' className={style.btn} onClick={props.onClick}>{props.title}</button>
+    </>
+  ) : (
+    <>
+    <button type='button' className={style.btnDisable} onClick={props.onClick}>{props.title}</button>
+    </>
+  )
+
+}
+
+export default ButtonModal;
