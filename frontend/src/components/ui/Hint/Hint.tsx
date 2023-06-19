7ab149075478172bc797b792e9ef5685e0b842e0
Vladislav Nikitin <vlad.nikitin1997@bk.ru>
2023-06-19 05:31:01 +0300

    feat: ToastPrintBarcode

--- frontend/src/components/ui/Hint/Hint.tsx
+++ frontend/src/components/ui/Hint/Hint.tsx
@@ -1,18 +1,30 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import style from './Hint.module.css';
 import pen from '../../../images/Icon_pen.svg';
 import lock from '../../../images/icon_lock.svg';
 import { IHint } from '../../../utils/type/main';
 import { closeBoxApi } from '../../../utils/api';
+import ToastPrintBarcode from '../../ToastPrintBarcode/ToastPrintBarcode';
 
 const Hint: FC<IHint> = ({icon, title, visible}) => {
+
+  const [isToastPrintBarcode, setToastPrintBarcode] = useState(false);
+
+  const openToastPrintBarcode = () => {
+    setToastPrintBarcode(true);
+  }
+
+  const closeToastPrintBarcode = () => {
+    setToastPrintBarcode(false);
+  }
+
 
   const closeBox = async () => {
     try {
       await closeBoxApi()
         .then(res => {
           if (res.status === 'ok') {
-            // Открывается Тост
+            setToastPrintBarcode(false);
           }
         })
     } catch(error) {
@@ -22,6 +34,7 @@ const Hint: FC<IHint> = ({icon, title, visible}) => {
   }
 
   return (
+    <>
     <li className={
       visible ? `${style.hintBtn} ${style.hintShadow}` :
       `${style.btnWrp} ${style.hintShadow}`
@@ -32,12 +45,14 @@ const Hint: FC<IHint> = ({icon, title, visible}) => {
           <p className={style.title}>{title}</p>
         </button>
       ) : (
-        <button className={style.btn} onClick={closeBox}>
+        <button className={style.btn} onClick={openToastPrintBarcode}>
           <img className={`${style.imgHint}`} src={lock} alt='Иконка замка' />
           <p className={style.title}>{title}</p>
         </button>
       )}
     </li>
+    <ToastPrintBarcode isOpen={isToastPrintBarcode} onClick={closeBox} onClose={closeToastPrintBarcode} />
+    </>
   )
 }
 
